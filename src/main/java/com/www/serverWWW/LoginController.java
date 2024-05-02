060d845161056e5078cf2bf2e65bfb86713fc7d2
Jakub <122745716+WielkiKuba@users.noreply.github.com>
2024-05-02 12:03:03 +0200

    Update LoginController.java

--- src/main/java/com/www/serverWWW/LoginController.java
+++ src/main/java/com/www/serverWWW/LoginController.java
@@ -3,6 +3,8 @@ package com.www.serverWWW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -19,5 +21,14 @@ public class LoginController {
     @GetMapping("/loginFailure")
     public String loginFailure() {
         return "failureLogin.html";
+    }
+    @PostMapping("/signUp")
+    public String newUser(@RequestParam String username,@RequestParam String password){
+        userService.createUser(username,password);
+        return "index.html";
+    }
+    @GetMapping("/signUpView")
+    public String signUpView(){
+        return "signUpView.html";
     }
 }
